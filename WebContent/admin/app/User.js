20ffbe953d5f5aceadf9102f84a9e76544626402
DaniWi <daniel.witsch@student.uibk.ac.at>
2015-12-04 14:59:04 +0100

    backend

---
+++ WebContent/admin/app/User.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Dani on 01.12.2015.
+ */
+var User = (function () {
+    function User(name, admin) {
+        this.name = name;
+        this.admin = admin;
+    }
+    return User;
+})();
+exports.User = User;
+//# sourceMappingURL=User.js.map
